5b705027f39ef1f54de79a505ee6ada25676b942
Alex Papadopoulos <papadopoulos.alexios@gmail.com>
2023-11-02 15:37:54 +0200

    change Board._dimension property to a class constant. Add standard board setup

--- board.py
+++ board.py
@@ -5,36 +5,39 @@ class Board:
 	W = 1
 	B = -1
 	EMPTY = 0
+	DIMENSION = 8
 
 	def __init__(self):
-		self._dimension = 8
 		self._game_board = []
-		for _ in range(self._dimension):
-			self._game_board.append([self.EMPTY] * self._dimension)
+		for _ in range(self.DIMENSION):
+			self._game_board.append([self.EMPTY] * self.DIMENSION)
+		# Set up Othello game board
+		self._game_board[3][3] = self._game_board[4][4] = self.W
+		self._game_board[4][3] = self._game_board[3][4] = self.B
 		self._last_player = self.W  # black always plays first
 		self._last_move = None
-		self._available_pieces = self._dimension ** 2
+		self._available_pieces = self.DIMENSION ** 2
 
 	def print_board(self):
 		# Print column numbers
 		print('\n  ', end='')
-		for j in range(self._dimension):
+		for j in range(self.DIMENSION):
 			print(f'  {j + 1}   ', end='')
-		print(f"\n  {6*self._dimension*'-'}")
+		print(f"\n  {6*self.DIMENSION*'-'}")
 
 		# Print rows
-		for i in range(self._dimension):
+		for i in range(self.DIMENSION):
 			print(f'{i + 1}|', end='')
-			for j in range(self._dimension):
+			for j in range(self.DIMENSION):
 				piece = ' '
 				if self._game_board[i][j] == self.W:
-					piece = 'X'
-				elif self._game_board[i][j] == self.B:
 					piece = 'O'
+				elif self._game_board[i][j] == self.B:
+					piece = 'X'
 				print(f'  {piece}  |', end='')
-			if i != self._dimension - 1:
+			if i != self.DIMENSION - 1:
 				print('\n')
-		print(f"\n  {6*self._dimension*'-'}\n")
+		print(f"\n  {6*self.DIMENSION*'-'}\n")
 
 	def get_children(self, letter):
 		return list()
@@ -47,8 +50,8 @@ class Board:
 		return self._available_pieces == 0
 
 	def is_valid_move(self, row, col):
-		return not (row < 0 or row >= self._dimension or
-    				col < 0 or col >= self._dimension or
+		return not (row < 0 or row >= self.DIMENSION or
+    				col < 0 or col >= self.DIMENSION or
     				self._game_board[row][col] != self.EMPTY)
 
 	def make_move(self, row, col, letter):
@@ -75,11 +78,11 @@ class Board:
 
 	@property
 	def dimension(self):
-		return self._dimension
+		return self.DIMENSION
 
 	@dimension.setter
 	def dimension(self, d):
-		self._dimension = d
+		self.DIMENSION = d
 
 	@property
 	def game_board(self):
