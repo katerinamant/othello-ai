10a5eb1212baa74f70ca1d6d2e1635e50e16101a
katerinamant <katerinamantaraki@gmail.com>
2024-03-06 20:58:37 +0200

    add two player othello gui

--- gui.py
+++ gui.py
@@ -1,23 +1,12 @@
 import PySimpleGUI as sg
-
-# layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
-
-# # Create the window
-# window = sg.Window("Demo", layout)
-
-# # Create an event loop
-# while True:
-#     event, values = window.read()
-#     # End program if user closes window or
-#     # presses the OK button
-#     if event == "OK" or event == sg.WIN_CLOSED:
-#         break
-
-# window.close()
+from board import Board
+from player import Player
 
 class GUI:
     def __init__(self) -> None:
         sg.theme("DarkBlue2")
+        self._board_window = None
+        self._used_cells = set()
 
     def welcome_window(self) -> tuple[str, str]:
         """
@@ -71,3 +60,117 @@ class GUI:
                     window["Start"].update(disabled=(diff_input == None) or (color_input == None))
 
         return (diff_input, color_input)
+
+    def game(self, board: Board, user_player: Player, ai_player: Player):
+        layout = [[sg.Text(f" ", key="title")]]
+
+        # Create board layout
+        game_board = board.game_board
+        for row in range(board.dimension):
+            new_row = []
+            for column in range(board.dimension):
+                key = (row, column)
+                piece = " "
+                if game_board[row][column] == Board.B:
+                    piece = "X"
+                    self._used_cells.add(key)
+                elif game_board[row][column] == Board.W:
+                    piece = "O"
+                    self._used_cells.add(key)
+                new_row.append(sg.Button(button_text=piece, size=(3, 2), key=key))
+            layout.append(new_row)
+
+        # Add score messages
+        layout.append([sg.Text(f"Black (X): {board.black_pieces}", key="black_pieces")])
+        layout.append([sg.Text(f"White (O): {board.white_pieces}", key="white_pieces")])
+
+        self._board_window = sg.Window(title="Othello AI", layout=layout, finalize=True)
+
+        while True:
+            # Game loop
+            available_moves = {Board.W, Board.B}
+
+            while available_moves:
+                if board.last_player == Board.W:
+                    player_name = "black"
+                    piece = Board.B
+                else:
+                    player_name = "white"
+                    piece = Board.W
+
+                if piece not in available_moves:
+                    print(f"Sorry, there are no available moves for {player_name}!")
+                    board.last_player *= -1
+                    continue
+
+                #print(f"Time for {player_name} to make a move!")
+                if piece == user_player.piece_val:
+                    self._board_window["title"].update(f"Time for {player_name} to make a move!")
+                    # Event loop
+                    while True:
+                        event, values = self._board_window.read()
+
+                        if event == sg.WIN_CLOSED:
+                            break
+
+                        if event not in self._used_cells:
+                            pieces_to_flip = board.is_valid_move(event[0], event[1], piece)
+                            if pieces_to_flip:
+                                board.make_move(event[0], event[1], piece, pieces_to_flip)
+
+                            # Update board layout
+                            game_board = board.game_board
+                            for row in range(board.dimension):
+                                for column in range(board.dimension):
+                                    key = (row, column)
+                                    piece = " "
+                                    if game_board[row][column] == Board.B:
+                                        piece = "X"
+                                        self._used_cells.add(key)
+                                    elif game_board[row][column] == Board.W:
+                                        piece = "O"
+                                        self._used_cells.add(key)
+                                    self._board_window[key].update(piece)
+
+                            # Update score messages
+                            self._board_window["black_pieces"].update(f"Black (X): {board.black_pieces}")
+                            self._board_window["white_pieces"].update(f"White (O): {board.white_pieces}")
+
+                        break
+
+                else:
+                    self._board_window["title"].update(f"Time for {player_name} to make a move!")
+                    # Event loop
+                    while True:
+                        event, values = self._board_window.read()
+
+                        if event == sg.WIN_CLOSED:
+                            break
+
+                        if event not in self._used_cells:
+                            pieces_to_flip = board.is_valid_move(event[0], event[1], piece)
+                            if pieces_to_flip:
+                                board.make_move(event[0], event[1], piece, pieces_to_flip)
+
+                            # Update board layout
+                            game_board = board.game_board
+                            for row in range(board.dimension):
+                                for column in range(board.dimension):
+                                    key = (row, column)
+                                    piece = " "
+                                    if game_board[row][column] == Board.B:
+                                        piece = "X"
+                                        self._used_cells.add(key)
+                                    elif game_board[row][column] == Board.W:
+                                        piece = "O"
+                                        self._used_cells.add(key)
+                                    self._board_window[key].update(piece)
+
+                            # Update score messages
+                            self._board_window["black_pieces"].update(f"Black (X): {board.black_pieces}")
+                            self._board_window["white_pieces"].update(f"White (O): {board.white_pieces}")
+
+                        break
+
+                available_moves = board.is_terminal()
+                break
--- main.py
+++ main.py
@@ -41,52 +41,7 @@ def main():
         ai_player = Player(minimax_depth, Board.B)
 
     board = Board()
-    board.print_board()
-
-    available_moves = {Board.W, Board.B}
-    while available_moves:
-        if board.last_player == Board.W:
-            player_name = "black"
-            piece = Board.B
-        else:
-            player_name = "white"
-            piece = Board.W
-
-        if piece not in available_moves:
-            print(f"Sorry, there are no available moves for {player_name}!")
-            board.last_player *= -1
-            continue
-
-        print(f"Time for {player_name} to make a move!")
-        if piece == user_player.piece_val:
-            while True:
-                try:
-                    x = int(input("Choose row: ")) - 1
-                    y = int(input("Choose column: ")) - 1
-                    pieces_to_flip = board.is_valid_move(x, y, piece)
-                    if pieces_to_flip:
-                        board.make_move(x, y, piece, pieces_to_flip)
-                        break
-                    else:
-                        print("Invalid move! Try again...")
-                except ValueError:
-                    print("Invalid input! Try again...\n")
-        else:
-            print("Calculating...")
-            alpha = float("-inf")
-            beta = float("inf")
-            move = ai_player.mini_max(board, 0, alpha, beta, piece)[1]
-            # We know it is a valid move
-            # but the is_valid_move() function
-            # also calculates the pieces to flip
-            pieces_to_flip = board.is_valid_move(move.row, move.col, piece)
-            board.make_move(move.row, move.col, piece, pieces_to_flip)
-            print(
-                f"{player_name[0].upper() + player_name[1:]} placed a piece on {move.row + 1} x {move.col + 1}"
-            )
-
-        board.print_board()
-        available_moves = board.is_terminal()
+    gui.game(board, user_player, ai_player)
 
     print("No valid moves for either player. The game is over.")
 
