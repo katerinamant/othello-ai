d9917a2373d8bde85d8a593cbca7c1f94cfada6d
katerinamant <katerinamantaraki@gmail.com>
2024-03-06 22:10:42 +0200

    added ai moves to gui + WindowClosedError

--- gui.py
+++ gui.py
@@ -2,6 +2,12 @@ import PySimpleGUI as sg
 from board import Board
 from player import Player
 
+
+class WindowClosedError(Exception):
+    """
+    The user unexpectedly closed the window
+    """
+
 class GUI:
     def __init__(self) -> None:
         sg.theme("DarkBlue2")
@@ -43,7 +49,11 @@ class GUI:
         while True:
             event, values = window.read()
 
-            if event == "Start" or event == sg.WIN_CLOSED:
+            if event == sg.WIN_CLOSED:
+                window.close()
+                raise WindowClosedError
+
+            if event == "Start":
                 window.close()
                 break
 
@@ -61,8 +71,20 @@ class GUI:
 
         return (diff_input, color_input)
 
-    def game(self, board: Board, user_player: Player, ai_player: Player):
-        layout = [[sg.Text(f" ", key="title")]]
+    def create_board_window(self, board: Board, ai_player: Player):
+        """
+        Creates the window with the game board
+        """
+        layout = []
+
+        if ai_player.piece_val == Board.B:
+            row = [sg.Text(f"Black is calculating a move...", key="title")]
+        else:
+            row = [sg.Text(f"Time for black to make a move!", key="title")]
+        # Add continue button
+        row.append(sg.Text('', size=(8, 1)))
+        row.append(sg.Button("Continue",disabled=ai_player.piece_val != Board.B))
+        layout.append(row)
 
         # Create board layout
         game_board = board.game_board
@@ -86,91 +108,100 @@ class GUI:
 
         self._board_window = sg.Window(title="Othello AI", layout=layout, finalize=True)
 
+    def user_move(self, board: Board, user_player: Player):
+        """
+        Updates the board when the user chooses
+        a cell to place a piece
+        """
+        if user_player.piece_val == Board.B:
+            player_name = "black"
+            ai_name = "White"
+        else:
+            player_name = "white"
+            ai_name = "Black"
+        self._board_window["title"].update(f"Time for {player_name} to make a move!")
+
+        self._board_window["Continue"].update(disabled=True)
+
+        self._board_window.refresh()
+
+        # Event loop
         while True:
-            # Game loop
-            available_moves = {Board.W, Board.B}
+            event, values = self._board_window.read()
 
-            while available_moves:
-                if board.last_player == Board.W:
-                    player_name = "black"
-                    piece = Board.B
-                else:
-                    player_name = "white"
-                    piece = Board.W
+            if event == sg.WIN_CLOSED:
+                self._board_window.close()
+                raise WindowClosedError
 
-                if piece not in available_moves:
-                    print(f"Sorry, there are no available moves for {player_name}!")
-                    board.last_player *= -1
-                    continue
+            if event not in self._used_cells:
+                pieces_to_flip = board.is_valid_move(event[0], event[1], user_player.piece_val)
+                if pieces_to_flip:
+                    board.make_move(event[0], event[1], user_player.piece_val, pieces_to_flip)
 
-                #print(f"Time for {player_name} to make a move!")
-                if piece == user_player.piece_val:
-                    self._board_window["title"].update(f"Time for {player_name} to make a move!")
-                    # Event loop
-                    while True:
-                        event, values = self._board_window.read()
+                    # Update board layout
+                    game_board = board.game_board
+                    for row in range(board.dimension):
+                        for column in range(board.dimension):
+                            key = (row, column)
+                            piece = " "
+                            if game_board[row][column] == Board.B:
+                                piece = "X"
+                                self._used_cells.add(key)
+                            elif game_board[row][column] == Board.W:
+                                piece = "O"
+                                self._used_cells.add(key)
+                            self._board_window[key].update(piece)
 
-                        if event == sg.WIN_CLOSED:
-                            break
+                    # Update score messages
+                    self._board_window["black_pieces"].update(f"Black (X): {board.black_pieces}")
+                    self._board_window["white_pieces"].update(f"White (O): {board.white_pieces}")
 
-                        if event not in self._used_cells:
-                            pieces_to_flip = board.is_valid_move(event[0], event[1], piece)
-                            if pieces_to_flip:
-                                board.make_move(event[0], event[1], piece, pieces_to_flip)
+                    # Update title
+                    self._board_window["title"].update(f"{ai_name} is calculating a move...")
 
-                            # Update board layout
-                            game_board = board.game_board
-                            for row in range(board.dimension):
-                                for column in range(board.dimension):
-                                    key = (row, column)
-                                    piece = " "
-                                    if game_board[row][column] == Board.B:
-                                        piece = "X"
-                                        self._used_cells.add(key)
-                                    elif game_board[row][column] == Board.W:
-                                        piece = "O"
-                                        self._used_cells.add(key)
-                                    self._board_window[key].update(piece)
+                self._board_window.refresh()
+                break
 
-                            # Update score messages
-                            self._board_window["black_pieces"].update(f"Black (X): {board.black_pieces}")
-                            self._board_window["white_pieces"].update(f"White (O): {board.white_pieces}")
+    def ai_move(self, board: Board, ai_player: Player, x: int, y: int):
+        """
+        Updates the board when the ai
+        makes a move
+        """
+        if ai_player.piece_val == Board.B:
+            player_name = "black"
+        else:
+            player_name = "white"
+        self._board_window["title"].update(f"{player_name[0].upper() + player_name[1:]} placed a piece on {x + 1} x {y + 1}")
 
-                        break
+        self._board_window["Continue"].update(disabled=False)
 
-                else:
-                    self._board_window["title"].update(f"Time for {player_name} to make a move!")
-                    # Event loop
-                    while True:
-                        event, values = self._board_window.read()
+        # Update board layout
+        game_board = board.game_board
+        for row in range(board.dimension):
+            for column in range(board.dimension):
+                key = (row, column)
+                piece = " "
+                if game_board[row][column] == Board.B:
+                    piece = "X"
+                    self._used_cells.add(key)
+                elif game_board[row][column] == Board.W:
+                    piece = "O"
+                    self._used_cells.add(key)
+                self._board_window[key].update(piece)
 
-                        if event == sg.WIN_CLOSED:
-                            break
+        # Update score messages
+        self._board_window["black_pieces"].update(f"Black (X): {board.black_pieces}")
+        self._board_window["white_pieces"].update(f"White (O): {board.white_pieces}")
 
-                        if event not in self._used_cells:
-                            pieces_to_flip = board.is_valid_move(event[0], event[1], piece)
-                            if pieces_to_flip:
-                                board.make_move(event[0], event[1], piece, pieces_to_flip)
+        self._board_window.refresh()
 
-                            # Update board layout
-                            game_board = board.game_board
-                            for row in range(board.dimension):
-                                for column in range(board.dimension):
-                                    key = (row, column)
-                                    piece = " "
-                                    if game_board[row][column] == Board.B:
-                                        piece = "X"
-                                        self._used_cells.add(key)
-                                    elif game_board[row][column] == Board.W:
-                                        piece = "O"
-                                        self._used_cells.add(key)
-                                    self._board_window[key].update(piece)
+        # Event loop
+        while True:
+            event, values = self._board_window.read()
 
-                            # Update score messages
-                            self._board_window["black_pieces"].update(f"Black (X): {board.black_pieces}")
-                            self._board_window["white_pieces"].update(f"White (O): {board.white_pieces}")
+            if event == sg.WIN_CLOSED:
+                self._board_window.close()
+                raise WindowClosedError
 
-                        break
-
-                available_moves = board.is_terminal()
+            if event == "Continue":
                 break
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from gui import GUI
+from gui import WindowClosedError
 from player import Player
 from board import Board
 
@@ -9,10 +10,9 @@ def welcome_screen() -> tuple[int, str]:
 
     """
     print("\tWelcome to Othello AI!")
-    difficulty, color = gui.welcome_window()
-
-    if difficulty == None or color == None:
-        # User closed the window without providing input
+    try:
+        difficulty, color = gui.welcome_window()
+    except WindowClosedError:
         return (None, None)
 
     if difficulty == "Novice":
@@ -41,7 +41,46 @@ def main():
         ai_player = Player(minimax_depth, Board.B)
 
     board = Board()
-    gui.game(board, user_player, ai_player)
+    gui.create_board_window(board, ai_player)
+
+    available_moves = {Board.W, Board.B}
+    while available_moves:
+        if board.last_player == Board.W:
+            player_name = "black"
+            piece = Board.B
+        else:
+            player_name = "white"
+            piece = Board.W
+
+        if piece not in available_moves:
+            print(f"Sorry, there are no available moves for {player_name}!")
+            board.last_player *= -1
+            continue
+
+        print(f"Time for {player_name} to make a move!")
+        if piece == user_player.piece_val:
+            try:
+                gui.user_move(board, user_player)
+            except WindowClosedError:
+                return
+        else:
+            alpha = float("-inf")
+            beta = float("inf")
+            move = ai_player.mini_max(board, 0, alpha, beta, ai_player.piece_val)[1]
+            # We know it is a valid move
+            # but the is_valid_move() function
+            # also calculates the pieces to flip
+            pieces_to_flip = board.is_valid_move(move.row, move.col, ai_player.piece_val)
+            board.make_move(move.row, move.col, ai_player.piece_val, pieces_to_flip)
+            print(
+                f"{player_name[0].upper() + player_name[1:]} placed a piece on {move.row + 1} x {move.col + 1}"
+            )
+            try:
+                gui.ai_move(board, ai_player, move.row, move._col)
+            except WindowClosedError:
+                return
+
+        available_moves = board.is_terminal()
 
     print("No valid moves for either player. The game is over.")
 
